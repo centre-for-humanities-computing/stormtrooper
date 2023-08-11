17faa992397b4a8a51579840e3c666bd4f26c907
Márton Kardos <power.up1163@gmail.com>
2023-08-11 09:50:23 +0200

    Added few-shot models to __init__

--- stormtrooper/__init__.py
+++ stormtrooper/__init__.py
@@ -1,3 +1,9 @@
-from stormtrooper.generative import GenerativeZeroShotClassifier
-from stormtrooper.text2text import Text2TextZeroShotClassifier
+from stormtrooper.generative import (
+    GenerativeFewShotClassifier,
+    GenerativeZeroShotClassifier,
+)
+from stormtrooper.text2text import (
+    Text2TextFewShotClassifier,
+    Text2TextZeroShotClassifier,
+)
 from stormtrooper.zero_shot import ZeroShotClassifier
