216c3135917b6c8c03e4c875995f49498b906f62
Márton Kardos <power.up1163@gmail.com>
2023-09-22 09:03:58 +0200

    Added TGI to __init__.py

--- stormtrooper/__init__.py
+++ stormtrooper/__init__.py
@@ -1,19 +1,15 @@
 from stormtrooper.error import NotInstalled
-from stormtrooper.generative import (
-    GenerativeFewShotClassifier,
-    GenerativeZeroShotClassifier,
-)
-from stormtrooper.text2text import (
-    Text2TextFewShotClassifier,
-    Text2TextZeroShotClassifier,
-)
+from stormtrooper.generative import (GenerativeFewShotClassifier,
+                                     GenerativeZeroShotClassifier)
+from stormtrooper.text2text import (Text2TextFewShotClassifier,
+                                    Text2TextZeroShotClassifier)
+from stormtrooper.text_gen_inference import (TGIFewShotClassifier,
+                                             TGIZeroShotClassifier)
 from stormtrooper.zero_shot import ZeroShotClassifier
 
 try:
-    from stormtrooper.set_fit import (
-        SetFitFewShotClassifier,
-        SetFitZeroShotClassifier,
-    )
+    from stormtrooper.set_fit import (SetFitFewShotClassifier,
+                                      SetFitZeroShotClassifier)
 except ModuleNotFoundError:
     SetFitZeroShotClassifier = NotInstalled(
         "SetFitZeroShotClassifier", "setfit"
@@ -21,10 +17,8 @@ except ModuleNotFoundError:
     SetFitFewShotClassifier = NotInstalled("SetFitFewShotClassifier", "setfit")
 
 try:
-    from stormtrooper.openai import (
-        OpenAIFewShotClassifier,
-        OpenAIZeroShotClassifier,
-    )
+    from stormtrooper.openai import (OpenAIFewShotClassifier,
+                                     OpenAIZeroShotClassifier)
 except ModuleNotFoundError:
     OpenAIZeroShotClassifier = NotInstalled(
         "OpenAIZeroShotClassifier", "openai"
@@ -41,4 +35,6 @@ __all__ = [
     "SetFitFewShotClassifier",
     "OpenAIFewShotClassifier",
     "OpenAIZeroShotClassifier",
+    "TGIZeroShotClassifier",
+    "TGIFewShotClassifier",
 ]
