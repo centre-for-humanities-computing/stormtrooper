cc7b0c4107a18553a3312176a1fd45f747a9790e
Márton Kardos <power.up1163@gmail.com>
2024-08-22 14:52:50 +0200

    Rewrote SetFit from scratch as the original library is a complete mess

--- stormtrooper/set_fit.py
+++ stormtrooper/set_fit.py
@@ -1,10 +1,11 @@
 from typing import Iterable, Optional
 
-import datasets
 import numpy as np
-from setfit import SetFitModel, Trainer, TrainingArguments, get_templated_dataset
+from sentence_transformers import SentenceTransformer
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.exceptions import NotFittedError
+from sklearn.linear_model import LogisticRegression
+from stormtrooper._setfit import generate_synthetic_samples, finetune_contrastive
 
 
 class SetFitClassifier(BaseEstimator, ClassifierMixin):
@@ -14,46 +15,48 @@ class SetFitClassifier(BaseEstimator, ClassifierMixin):
     ----------
     model_name: str, default 'sentence-transformers/all-MiniLM-L6-v2'
         Name of the encoder model.
-    sample_size: int, default 8
-        Number of  training samples to generate (only taken into account when zero-shot)
     device: str, default 'cpu'
         Device to train and run the model on.
+    classification_head: ClassifierMixin, default None
+        Classifier to use as the last step.
+        Defaults to Logistic Regression when not specified.
     n_epochs: int, default 10
         Number of trainig epochs.
-    batch_size: int, default 32
+    batch_size: int, default 8
         Batch size to use during training.
+    sample_size: int, default 8
+        Number of  training samples to generate (only zero-shot)
+    template_sentence: str, default "This sentence is {label}"
+        Template sentence for synthetic samples (only zero-shot)
+    random_state: int, default 42
+        Seed to use for stochastic training.
     """
 
     def __init__(
         self,
         model_name: str = "sentence-transformers/all-MiniLM-L6-v2",
-        sample_size: int = 8,
+        classification_head: Optional[ClassifierMixin] = None,
         device: str = "cpu",
         n_epochs: int = 10,
         batch_size: int = 32,
+        sample_size: int = 8,
+        template_sentence: str = "This sentence is {label}.",
+        random_state: int = 42,
     ):
         self.model_name = model_name
         self.classes_ = None
-        self.model = SetFitModel.from_pretrained(model_name)
-        self.model.model_body.to(device)
+        self.template_sentence = template_sentence
+        self.random_state = random_state
+        self.encoder = SentenceTransformer(model_name, device=device)
+        if classification_head is None:
+            self.classification_head = LogisticRegression()
+        else:
+            self.classification_head = classification_head
         self.trainer = None
         self.sample_size = sample_size
         self.device = device
         self.n_epochs = n_epochs
         self.batch_size = batch_size
-
-    def _get_train_dataset(self) -> datasets.Dataset:
-        if getattr(self, "examples_", None) is not None:
-            X = []
-            y = []
-            for label, xs in self.examples_.items():
-                X.extend(xs)
-                y.extend([label] * len(xs))
-            return datasets.Dataset.from_dict(dict(text=X, label=y))
-        else:
-            return get_templated_dataset(
-                candidate_labels=list(self.classes_), sample_size=self.sample_size
-            )
 
     def fit(self, X: Optional[Iterable[str]], y: Iterable[str]):
         if X is not None:
@@ -62,32 +65,32 @@ class SetFitClassifier(BaseEstimator, ClassifierMixin):
                 if label not in self.examples_:
                     self.examples_[label] = []
                 self.examples_[label].append(text)
-            self.classes_ = np.array(list(self.examples_.keys()))
-        else:
-            self.classes_ = np.array(list(set(y)))
-        self.n_classes = len(self.classes_)
-        self.trainer = Trainer(
-            model=self.model,
-            args=TrainingArguments(
-                num_epochs=self.n_epochs, batch_size=self.batch_size
-            ),
-            train_dataset=self._get_train_dataset(),
+        if X is None:
+            X, y = generate_synthetic_samples(
+                y,
+                n_sample_per_label=self.sample_size,
+                template_sentence=self.template_sentence,
+            )
+        self.encoder = finetune_contrastive(
+            self.encoder,
+            X,
+            y,
+            n_epochs=self.n_epochs,
+            seed=self.random_state,
         )
-        self.trainer.train()
+        X = list(X)
+        X_embeddings = self.encoder.encode(X)
+        self.classification_head.fit(X_embeddings, y)
+        self.classes_ = self.classification_head.classes_
+        self.n_classes = len(self.classes_)
         return self
 
     def predict(self, X: Iterable[str]) -> np.ndarray:
-        X = list(X)
         if getattr(self, "classes_", None) is None:
             raise NotFittedError("You need to fit the model before running inference.")
-        if getattr(self, "examples_", None) is None:
-            pred_ind = self.model(X)
-            pred = self.classes_[pred_ind]
-            # Produces a single str when one example is passed
-            # and we do not like that.
-            if isinstance(pred, str):
-                pred = np.array([pred])
-            return pred
+        return self.classification_head.predict(self.encoder.encode(X))
 
-        else:
-            return np.array(self.model(X))
+    def predict_proba(self, X: Iterable[str]) -> np.ndarray:
+        if getattr(self, "classes_", None) is None:
+            raise NotFittedError("You need to fit the model before running inference.")
+        return self.classification_head.predict_proba(self.encoder.encode(X))
